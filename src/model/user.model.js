7cee9e0bd1a1a6031087e938ca911560b65a3808
seilah <princenoru@gmail.com>
2025-12-04 18:15:18 +0100

    homework-accounting-password-hash: updated project with code from lesson

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, {Types} from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
         _id: {
@@ -40,5 +41,12 @@ const userSchema = new mongoose.Schema({
         }
     }
 );
+
+userSchema.pre('save', async function () {
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(12);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+})
 
 export default mongoose.model('User', userSchema, 'users');
